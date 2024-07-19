b360c40e70f27e7b60c6f4dacbd15d9bfb48c9b6
Antoni Pokusinski <apokusinski@o2.pl>
2024-07-19 22:07:16 +0200

    package/utils-raspberrypi: pin commit, update hash

--- package/utils-raspberrypi/utils-raspberrypi.mk
+++ package/utils-raspberrypi/utils-raspberrypi.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-UTILS_RASPBERRYPI_SOURCE = master.tar.gz
-UTILS_RASPBERRYPI_SITE = https://github.com/raspberrypi/utils/archive/refs/heads
+UTILS_RASPBERRYPI_VERSION = 88867f61b0aab512b8ee9ec9ee67751820ac43e5
+UTILS_RASPBERRYPI_SITE = $(call github,raspberrypi,utils,$(UTILS_RASPBERRYPI_VERSION))
 UTILS_RASPBERRYPI_LICENSE = BSD-3-Clause
 UTILS_RASPBERRYPI_LICENSE_FILES = LICENCE
 UTILS_RASPBERRYPI_DEPENDENCIES = dtc
